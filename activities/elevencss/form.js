a3ad4c2713f341e8e4cf55c24247402e44d1a56f
Steffi Cheung <stefficheungws@gmail.com>
2025-11-18 05:44:10 +0800

    Form Processing with AJAX

---
+++ activities/elevencss/form.js
@@ -0,0 +1,45 @@
+document.getElementById('myForm').addEventListener('submit',function(event){
+    event.preventDefault();
+    
+
+    const fullname = document.getElementById('fname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+    const age = document.getElementById('age').value;
+
+    if (!fullname || !email){
+        alert("You need a name and email.");
+        return;
+    }
+
+    if (!age || age <18) {
+        alert("You need to be 18");
+        return;
+    }
+
+    const formData = {
+        name: fullname,
+        email: email,
+        password: password,
+        age: age
+    };
+
+    alert("Form Submitted");
+    console.log(formData);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 && xhr.status === 200){
+            alert("Form submitted successfully!");
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+            document.getElementById('myForm').innerHTML = '';
+            document.getElementById('message').innerText = response.message;
+        } else if (xhr.readyState === 4){
+            alert("Error submitting form.");
+        }
+    }
+    xhr.send(JSON.stringify(formData));
+});
